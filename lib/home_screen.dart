e543053ca833dce3e9e74b493ba68a3d6925841f
SuhwanCha <suhw4n@gmail.com>
2023-12-09 16:11:03 +0900

    feat: firebase auth ui

---
+++ lib/home_screen.dart
@@ -0,0 +1,34 @@
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:flutter/material.dart';
+
+final _flowKey = Object();
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Christmassy'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: FirebaseUIActions(
+          actions: [
+            SMSCodeRequestedAction((context, action, flowKey, phoneNumber) {
+              Navigator.of(context).push(
+                MaterialPageRoute<void>(
+                  builder: (context) => SMSCodeInputScreen(
+                    flowKey: flowKey,
+                  ),
+                ),
+              );
+            }),
+          ],
+          child: PhoneInputView(flowKey: _flowKey),
+        ),
+      ),
+    );
+  }
+}
